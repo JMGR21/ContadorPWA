ef24680347b7642f616170da3112565b4f5ad87c
josmi <jtutosgr@gmail.com>
2022-03-03 11:16:08 -0600

    PWA App

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'Counter App',
+            count: 0
+        }
+    }, 
+    methods: {
+        modCount(instruction = "add", limit = 1) {
+            if(instruction === 'dis') this.count -= limit;
+            else this.count += limit; 
+        }
+    }
+});
